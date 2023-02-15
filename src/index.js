febd5281cd0fcbac41e360bde8fa686766dbaae1
DenMerch <rosignolli@gmail.com>
2023-02-15 20:29:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,61 +12,61 @@ let input = "";
 
 formEl.addEventListener("input", debounce((handleInput), 100));
 function handleInput(event) {
-    input = event.target.value.trim(" ");
+  input = event.target.value.trim(" ");
 
 }
 formEl.addEventListener("submit", (ev) => {
-    handleSubmit(ev);
+  handleSubmit(ev);
 
 });
 loadBtnEl.addEventListener("click", handleBtn);
 function handleBtn() {
-    let dataArr = [];
-    fetchData(input, currentFetchPage)
-        .then((data) => {
-            dataArr = data.data.hits;
-            drawing(dataArr)
-            currentFetchPage++;
-        })
-        .catch((er) => {
-            Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-        });
+  let dataArr = [];
+  fetchData(input, currentFetchPage)
+    .then((data) => {
+      dataArr = data.data.hits;
+      drawing(dataArr)
+      currentFetchPage++;
+    })
+    .catch((er) => {
+      Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+    });
 
 }
 function handleSubmit(ev) {
-    ev.preventDefault();
-    currentFetchPage = 1;
-    galeryEl.innerHTML = "";
-    let dataArr = [];
-    fetchData(input, currentFetchPage)
-        .then((data) => {
-            let totalHits = data.data.totalHits;
-            if (totalHits === 0) {
-                return Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-            }
-            else if (totalHits === document.querySelectorAll(".photo-card").length + 1) {
-                return Notiflix.Notify.info("Hooray! We found totalHits images.")
-            }
-            Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`)
-            dataArr = data.data.hits;
-            drawing(dataArr)
-            loadBtnEl.classList.remove("disabled");
-            currentFetchPage++;
+  ev.preventDefault();
+  currentFetchPage = 1;
+  galeryEl.innerHTML = "";
+  let dataArr = [];
+  fetchData(input, currentFetchPage)
+    .then((data) => {
+      let totalHits = data.data.totalHits;
+      if (totalHits === 0) {
+        return Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+      }
+      else if (totalHits === document.querySelectorAll(".photo-card").length + 1) {
+        return Notiflix.Notify.info("Hooray! We found totalHits images.")
+      }
+      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`)
+      dataArr = data.data.hits;
+      drawing(dataArr)
+      loadBtnEl.classList.remove("disabled");
+      currentFetchPage++;
 
-        }
+    }
 
-        )
-        .catch((er) => {
-            Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-        }
+    )
+    .catch((er) => {
+      Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+    }
 
-        )
-        .finally(ev.currentTarget.reset());
+    )
+    .finally(ev.currentTarget.reset());
 
 }
 
 function markupGallery(arr) {
-    return arr.map(item => `<div class="photo-card">
+  return arr.map(item => `<div class="photo-card">
     <a class="gallery__item" href="${item.largeImageURL}">
     <img class="gallery__image" src="${item.webformatURL}" alt="${item.tags}"  />
 </a>
@@ -87,8 +87,7 @@ function markupGallery(arr) {
 </div>`).join("");
 }
 function drawing(params) {
-    galeryEl.insertAdjacentHTML("beforeend", markupGallery(params));
-    let gallery = new SimpleLightbox('.gallery a', { captionDelay: 250 });
-    gallery.refresh();
+  galeryEl.insertAdjacentHTML("beforeend", markupGallery(params));
+  let gallery = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+  gallery.refresh();
 }
-
